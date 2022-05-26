151db558024b81e394960a9df0583421b010a207
vinodh1988 <k.c.vinodh@hotmail.com>
2022-05-26 10:59:06 +0530

    new-commit-first

---
+++ FirstProject/src/com/sterling/Operators.java
@@ -0,0 +1,23 @@
+package com.sterling;
+
+public class Operators {
+   public static void main(String n[]) {
+	   // * / + - %
+	   
+	   System.out.println("12%5="+12%5);
+	   System.out.println("23+3*5 = "+(23+3*5));
+	   
+	   // > < >= <= != ==
+	   
+	   System.out.println(2==3);
+	   System.out.println(2!=5);
+	   System.out.println(2>=5);
+	   System.out.println(2<=3);
+	   
+	   // &&  ||  !
+	   
+	   System.out.println(2>3 && 2<3);
+	   System.out.println(3>2 || 3>5);
+	   System.out.println(!(3>2));
+   }
+}
