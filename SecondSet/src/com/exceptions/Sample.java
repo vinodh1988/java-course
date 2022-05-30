06990bcaa0fd630841e97926110e3534ef7d9cad
vinodh1988 <k.c.vinodh@hotmail.com>
2022-05-30 11:38:45 +0530

    exception-commit

---
+++ SecondSet/src/com/exceptions/Sample.java
@@ -0,0 +1,63 @@
+package com.exceptions;
+
+import java.util.Scanner;
+
+public class Sample {
+  public static void main(String n[]) {
+	Scanner sc=new Scanner(System.in);
+	 try {
+	  System.out.println(n[0]);
+	  System.out.println(n[2]);
+	  System.out.println("Enter two integers");
+	    int a= Integer.parseInt(sc.next());
+	    int b= Integer.parseInt(sc.next());
+	    System.out.println(a/b);
+	}
+	catch(ArrayIndexOutOfBoundsException e) {
+		e.printStackTrace();
+	}
+	catch(NullPointerException e) {
+		System.out.println("Null pointer exception");
+	}
+	
+	catch(RuntimeException e) {
+		System.out.println("Runtime exception occured and its type is"+e.getClass().getName());
+	}
+	  System.out.println("Some other logic");
+  }
+}
+
+
+/*
+   Problems that occur during runtime are called as Exceptions
+   
+   and the runtime problems may be due to 
+   
+   1. Logical issues
+  Anticipatable
+  
+   2. Hardware issues
+   3. Software issues [connectivity,availability]
+   
+   Unchecked exceptions 
+                 subclasses for runtime exceptions
+                 
+   checked exceptions
+                      direct subclasses of Exception
+                      
+                     
+         if exceptions  not handled...it terminates the program abruptly
+         
+         so to avoid that we need to handle it
+         
+         to handle exceptions , we use five keywords in java
+         
+         try
+         throw
+         catch
+         throws
+         finally
+                      
+                      
+
+*/
